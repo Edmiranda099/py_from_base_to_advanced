c159a9d3a1e951c133e77f455238770c718576f6
Edson Miranda Pimenta Júnior <edmiranda099@gmail.com>
2025-11-15 17:39:25 -0300

    comentarios sobre tuplask

--- tuplas.py
+++ tuplas.py
@@ -1,1 +1,9 @@
-print("efetuando o teste!!!")
+"""
+Tuplas (tuple)
+
+Tuplas são basicamente duas diferenças básicas:
+
+1 - As tuplas são representadas por parênteses ()
+
+2 - As tuplas são imutáveis: Isso significa que ao se criar uma tupla ela não muda. Toda operação em uma nova tupla.
+"""
